4d1875ffabdddb3f9e5cb6082bf90df6e0b09615
LiPolymer <fish@lipoly.ink>
2026-04-29 15:30:44 +0800

    feat: mbed support

--- src/BladeTask.hpp
+++ src/BladeTask.hpp
@@ -5,8 +5,15 @@
 #ifndef BLADETASK
 #define BLADETASK
 
+
 #ifdef IDF_VER
 #include <freertos/task.h>
+#endif
+
+#ifdef MBED_BUILD_TIMESTAMP
+#include <Thread.h>
+#include <chrono>
+#include <mbed_events.h>
 #endif
 
 #include <atomic>
@@ -76,15 +83,10 @@ public:
         if (isRunning) {
             isShutdownRequested = true;
             if (cleanBlockedStatus) xTaskAbortDelay(_taskHandle);
-            unsigned int elapsed = 0;
-            while (isRunning) {
-                if (elapsed > timeoutMs && timeoutMs != 0) {
-                    this->kill();
-                    _taskHandle = nullptr;
-                    return false;
-                }
-                vTaskDelay(pdMS_TO_TICKS(checkAlivePeriodMs));
-                if (timeoutMs != 0) elapsed += checkAlivePeriodMs;
+            if (!wait(timeoutMs, checkAlivePeriodMs)) {
+                this->kill();
+                _taskHandle = nullptr;
+                return false;
             }
         }
         return true;
@@ -105,16 +107,78 @@ public:
     }
 
     void kill() {
+        if (!isRunning) return;
         vTaskDelete(_taskHandle);
         isShutdownRequested = false;
         isRunning = false;
     }
 #endif
+#ifdef  MBED_BUILD_TIMESTAMP
+    std::unique_ptr<rtos::Thread> _thread = nullptr;
 
+    static void suicideAgent(BladeTask* task) {
+        delete task;
+    }
+
+    void processBootstrap() {
+        isRunning = true;
+
+        if (payload != nullptr) payload->payload(this);
+
+        isShutdownRequested = false;
+        isRunning = false;
+        if (isOneShoot && !isDestroying) {
+            mbed::mbed_event_queue() ->
+                call(mbed::callback(suicideAgent), this); //甩全局队列里防止左手砍右手
+        } //todo: 测试这个 不知道会不会炸
+    }
+
+    void start(const bool oneShoot = false) {
+        if (isRunning) return;
+        isOneShoot = oneShoot;
+        _thread = std::make_unique<rtos::Thread>();
+        _thread->start(mbed::callback(this, &BladeTask::processBootstrap));
+    }
+
+    bool stop(const unsigned int timeoutMs = 0, const unsigned short int checkAlivePeriodMs = 5,
+        const bool cleanBlockedStatus = false) {
+        if (isRunning) {
+            isShutdownRequested = true;
+            if (!wait(timeoutMs, checkAlivePeriodMs)) {
+                this->kill();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool wait(const unsigned int timeoutMs = 0, const unsigned short int checkAlivePeriodMs = 5) {
+        if (isRunning) {
+            if (timeoutMs == 0) _thread->join();
+            else { //todo: 考虑使用eventFlag
+                unsigned int elapsed = 0;
+                while (isRunning) {
+                    if (elapsed > timeoutMs) {
+                        return false;
+                    }
+                    rtos::ThisThread::sleep_for(std::chrono::milliseconds(checkAlivePeriodMs));
+                    elapsed += checkAlivePeriodMs;
+                }
+            }
+        }
+        return true;
+    }
+
+    void kill() {
+        if (!isRunning) return;
+        _thread->terminate();
+        isShutdownRequested = false;
+        isRunning = false;
+    }
+#endif
     ~BladeTask() {
         isDestroying = true;
         this->stop(0);
     }
 };
-
-#endif //BLADETASK
+#endif
